462b8ffb929202c6c7d65d677fd4383343d08051
Yifei Wang <ywab2018@mymail.pomona.edu>
2021-07-20 13:42:03 +0800

    created city column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_093800) do
+ActiveRecord::Schema.define(version: 2021_07_20_054057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_07_19_093800) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "city"
     t.index ["user_id"], name: "index_performances_on_user_id"
   end
 
