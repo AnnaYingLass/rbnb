04e87c053fd2db4aa577d4bcce78d10799fe62ed
Yifei Wang <ywab2018@mymail.pomona.edu>
2021-07-20 13:44:13 +0800

    created highlight column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_054057) do
+ActiveRecord::Schema.define(version: 2021_07_20_054259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_07_20_054057) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "city"
+    t.string "highlight"
     t.index ["user_id"], name: "index_performances_on_user_id"
   end
 
